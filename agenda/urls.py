3bfe68a948c70439887918e7ff810ba64e43b3a9
calebewerneckcouto <calebewerneck@hotmail.com>
2025-01-14 17:11:22 -0300

    Navegando entre paginas da aplicação

--- agenda/urls.py
+++ agenda/urls.py
@@ -4,7 +4,7 @@ from agenda.views import exibir_evento
 from agenda.views import listar_eventos
 
 urlpatterns=[
-    path("", listar_eventos, name="listar_eventos"),
-     path("evento/<int:id>/",exibir_evento)
+    path("eventos/", listar_eventos, name="listar_eventos"),
+     path("evento/<int:id>/",exibir_evento,name="exibir_evento")
       
 ]
