291c5659c48bb6f3f01d565a427c55551434741b
calebewerneckcouto <calebewerneck@hotmail.com>
2025-01-14 17:29:29 -0300

    enviando um formulario

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from agenda.views import exibir_evento
+from agenda.views import exibir_evento,listar_eventos,participar_evento
 
-from agenda.views import listar_eventos
+
 
 urlpatterns=[
     path("eventos/", listar_eventos, name="listar_eventos"),
-     path("evento/<int:id>/",exibir_evento,name="exibir_evento")
+    path("evento/<int:id>/",exibir_evento,name="exibir_evento"),
+    path("participar/",participar_evento,name="participar_evento")
       
 ]
--- agenda/views.py
+++ agenda/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,get_object_or_404
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse,HttpResponseRedirect
 from django.template import loader
 from agenda.models import Evento
 from datetime import date
+from django.urls import reverse
 
 # Create your views here.
 def listar_eventos(request):
@@ -16,3 +17,13 @@ def listar_eventos(request):
 def exibir_evento(request, id):
     evento = get_object_or_404(Evento, id=id)
     return render(request, "agenda/exibir_evento.html", {"evento": evento})
+
+
+def participar_evento(request):
+    evento_id = request.POST.get("evento_id")
+    evento = get_object_or_404(Evento,id=evento_id)
+    evento.participantes +=1
+    evento.save()
+    return HttpResponseRedirect(reverse('exibir_evento',args=(evento_id)))
+    
+    
