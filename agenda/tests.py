56bec814c9edf3fd58ea84c6490965e18d8ed0f2
calebewerneckcouto <calebewerneck@hotmail.com>
2025-01-17 17:05:29 -0300

    escrevendo testes automatizados

--- agenda/tests.py
+++ agenda/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase,Client
+from agenda.models import Evento,Categoria
+from datetime import date
+
 
 # Create your tests here.
+
+class TestPaginaInicial(TestCase):
+    def test_lista_eventos(self):
+        client = Client()
+        response=client.get("/")
+       #self.assertContains(response,"<th>Nome</th>") # verifica se a pagina esta renderizando isso /se tiver e pq esta funcionando...
+        self.assertTemplateUsed(response,"agenda/listar_eventos.html") # verifica se esta renderizando o template certo
+ 
+ 
+class TestListagemDeEventos(TestCase):
+    def test_evento_com_data_de_hoje_e_exibido(self):  
+        categoria = Categoria()
+        categoria.name="Machine Learning"
+        categoria.save()
+             
+        evento = Evento()
+        evento.nome="Aula de Inteligencia Artificial"
+        evento.categoria =categoria
+        evento.local="Santa Catarina"
+        evento.data=date.today()
+        evento.save()
+        
+        client = Client()
+        response=client.get("/")
+        self.assertContains(response,"Aula de Inteligencia Artificial")
+        self.assertContains(response,"A Definir")
+        self.assertEqual(response.context["eventos"][0],evento)
--- agenda/urls.py
+++ agenda/urls.py
@@ -4,7 +4,8 @@ from agenda.views import exibir_evento,listar_eventos,participar_evento
 
 
 urlpatterns=[
-    path("eventos/", listar_eventos, name="listar_eventos"),
+    path("", listar_eventos, name="listar_eventos_raiz"),
+     path("eventos/", listar_eventos, name="listar_eventos"),
     path("evento/<int:id>/",exibir_evento,name="exibir_evento"),
     path("participar/",participar_evento,name="participar_evento")
       
